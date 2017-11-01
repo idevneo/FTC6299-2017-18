d309f066bac731758aeb7d66e876966ec6545d36
Agentduble07 <alexander.goh.tan@gmail.com>
2017-11-01 11:43:47 -0500

    Revert "disabled other teleops and commented out code"
    This reverts commit 65681d81b10eae7ffc1a5a11c28125a565932f0a.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/ExampleTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/ExampleTeleop.java
@@ -51,7 +51,7 @@ import org.firstinspires.ftc.teamcode.Library.MyOpMode;
  * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
-@Disabled
+
 @TeleOp(name="example", group="Linear Opmode")
 public class ExampleTeleop extends MyOpMode {
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Teleop.java
@@ -54,7 +54,7 @@ import org.firstinspires.ftc.teamcode.Library.MyOpMode;
  * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
-@Disabled
+
 @TeleOp(name="old tele", group="Linear Opmode")
 public class Teleop extends MyOpMode {
 
