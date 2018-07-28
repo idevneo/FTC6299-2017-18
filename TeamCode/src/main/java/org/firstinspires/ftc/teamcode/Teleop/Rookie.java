25c81be3055d77f0afa28a369c54fb65d61a48d3
idevneo <idevneo@gmail.com>
2018-07-27 22:15:18 -0500

    Add files via upload
    Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Rookie.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Rookie.java
@@ -47,7 +47,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
  * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
-@TeleOp(name="DriveTrainAlt", group="Linear Opmode")
+@TeleOp(name="Rookie", group="Linear Opmode")
 @Disabled
 public class Rookie extends LinearOpMode {
 
